203b0796f983579e3a8ca05064a8bd65b552cee1
AMHBI <alimhbi1@gmail.com>
2024-03-02 18:18:12 +0330

    install uuid and develop contactList comp

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -1,35 +1,28 @@
 import { useState } from "react";
-import { ToastContainer, toast } from "react-toastify";
+import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-
+import { v4 } from "uuid";
 import ContactsList from "./ContactsList.jsx";
+
+import inputs from "../utils/inputs.js";
+import showToast from "../utils/showToast.js";
 
 function Contacts() {
   const [contacts, setContacts] = useState([]);
   const [contact, setContact] = useState({
+    id: "",
     name: "",
     lastName: "",
     email: "",
     phone: "",
   });
-  const showToast = (alert, type) => {
-    if (type === "error") {
-      toast.error(alert, {
-        position: "top-center",
-        theme: "colored",
-      });
-    }else if(type==="success"){
-        toast.success(alert, {
-            position: "top-center",
-            theme: "colored",
-          });
-    }
-  };
+
   const changeHandler = (event) => {
     const name = event.target.name;
     const value = event.target.value;
-    setContact((contact) => ({ ...contact, [name]: value }));
+    setContact((contact) => ({ ...contact, [name]: value, id: v4() }));
   };
+
   const addHandler = () => {
     if (
       !contact.name ||
@@ -37,7 +30,7 @@ function Contacts() {
       !contact.email ||
       !contact.phone
     ) {
-      showToast("All fields must be filled","error");
+      showToast("All fields must be filled", "error");
       return;
     }
 
@@ -52,42 +45,28 @@ function Contacts() {
     console.log(contacts);
   };
   return (
-    <div>
-      <div className="*:w-full md:*:w-[45%] *:m-3 *:h-12 *:p-2 *:pl-5 *:hover:outline-none *:active:outline-none
+    <div className='flex flex-col justify-center items-center mt-16'>
+      <div
+        className='*:w-full md:*:w-[45%] *:m-3 *:h-12 *:p-2 *:pl-5 *:hover:outline-none *:active:outline-none
        *:bg-accent *:rounded-lg *:text-primary *:placeholder:text-primary 
-      flex flex-wrap items-center justify-evenly ">
-        <input
-          name='name'
-          type='text'
-          placeholder='Name'
-          value={contact.name}
-          onChange={changeHandler}
-        />
-        <input
-          name='lastName'
-          type='text'
-          placeholder='Last Name'
-          value={contact.lastName}
-          onChange={changeHandler}
-        />
-        <input
-          name='email'
-          type='email'
-          placeholder='E-mail'
-          value={contact.email}
-          onChange={changeHandler}
-        />
-        <input
-          name='phone'
-          type='number'
-          inputMode='numeric'
-          placeholder='Phone'
-          value={contact.number}
-          onChange={changeHandler}
-        />
+      flex flex-wrap items-center justify-evenly '>
+        {inputs.map((input, index) => (
+          <input
+            key={index}
+            name={input.name}
+            type={input.type}
+            placeholder={input.placeholder}
+            value={contact[input.name]}
+            onChange={changeHandler}
+          />
+        ))}
       </div>
-        <ToastContainer />
-        <button onClick={addHandler}>Add Contact</button>
+      <ToastContainer />
+      <button
+        className='w-[30dvw] bg-primary text-white m-10 h-16 rounded-lg'
+        onClick={addHandler}>
+        Add Contact
+      </button>
       <ContactsList contacts={contacts} />
     </div>
   );
--- src/components/ContactsList.jsx
+++ src/components/ContactsList.jsx
@@ -1,8 +1,37 @@
+import avatar from "../assets/icons/user.svg";
+import trash from "../assets/icons/delete.svg";
+import email from "../assets/icons/email.svg";
+import phone from "../assets/icons/phone.svg";
 
-function ContactsList() {
+function ContactsList({ contacts }) {
   return (
-    <div>ContactsList</div>
-  )
+    <div className="w-full flex flex-col justify-center items-center gap-4">
+      <h2>Contacts</h2>
+      <ul className="contacts_list">
+        {contacts.map((contact) => (
+          <li key={contact.id}>
+            <div>
+              <img src={avatar} alt='' />
+              <p>
+                Name: {contact.name} {contact.lastName}
+              </p>
+            </div>
+            <div>
+              <img src={email} alt='Email' />
+              <p>E-mail: {contact.email}</p>
+            </div>
+            <div>
+            <img src={phone} alt="Phone" />
+            <p>Phone Number: {contact.phone}</p>
+            </div>
+            <div>
+              <img src={trash} alt="delete" />
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default ContactsList
+export default ContactsList;
